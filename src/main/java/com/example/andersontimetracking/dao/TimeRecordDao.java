76ec3b197fa0d63d8cbe1cf1b2ce1ad2c86325a1
Denys Polukhin <phoenix.snpd@gmail.com>
2023-09-26 11:40:03 +0300

    Added dao and getters and setters to TimeRecord

---
+++ src/main/java/com/example/andersontimetracking/dao/TimeRecordDao.java
@@ -0,0 +1,52 @@
+package com.example.andersontimetracking.dao;
+
+import com.example.andersontimetracking.JdbcConnector;
+import com.example.andersontimetracking.models.TimeRecord;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeRecordDao {
+    private final JdbcConnector connector;
+
+    public TimeRecordDao() {
+        connector = new JdbcConnector();
+    }
+
+    public List<TimeRecord> getAllRecords() {
+        List<TimeRecord> records = new ArrayList<>();
+        String getAllSql = "SELECT date, name, description, totaltime FROM timerecords";
+        try(Connection connection = connector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(getAllSql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                LocalDate date = resultSet.getDate("date").toLocalDate();
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                double totalTime = resultSet.getDouble("total_time");
+                TimeRecord record = new TimeRecord(date, name, description, totalTime);
+                records.add(record);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+    public void addRecord(TimeRecord record) {
+        String addRecordSql = "INSERT INTO timerecords VALUES (?, ?, ?, ?)";
+        Date sqlDate = Date.valueOf(record.getDate());
+        try(Connection connection = connector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(addRecordSql)) {
+            statement.setDate(1, sqlDate);
+            statement.setString(2, record.getName());
+            statement.setString(3, record.getDescription());
+            statement.setDouble(4, record.getTotalTime());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/andersontimetracking/models/TimeRecord.java
+++ src/main/java/com/example/andersontimetracking/models/TimeRecord.java
@@ -16,6 +16,46 @@ public class TimeRecord {
         this.totalTime = totalTime;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
     @Override
     public String toString() {
         return "TimeRecord{" +
