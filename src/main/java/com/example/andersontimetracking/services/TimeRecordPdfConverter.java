edd48ef95275588818d5e0117229cf8174daa7cb
Denys Polukhin <phoenix.snpd@gmail.com>
2023-09-26 12:20:37 +0300

    Chenged HelloServlet

--- src/main/java/com/example/andersontimetracking/services/TimeRecordPdfConverter.java
+++ src/main/java/com/example/andersontimetracking/services/TimeRecordPdfConverter.java
@@ -1,4 +1,11 @@
 package com.example.andersontimetracking.services;
 
+import com.example.andersontimetracking.dao.TimeRecordDao;
+
 public class TimeRecordPdfConverter {
+    private TimeRecordDao recordDao;
+
+    public TimeRecordPdfConverter(TimeRecordDao recordDao) {
+        recordDao = new TimeRecordDao();
+    }
 }
