d4590179a147e7a7064b9ac643874e638e51acb2
Tural <turalcode@gmail.com>
2023-09-27 10:31:35 +0400

    First Commit

--- src/main/java/com/example/andersontimetracking/servlets/HelloServlet.java
+++ src/main/java/com/example/andersontimetracking/servlets/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello my dear fullstack developers :P";
+        message = "Hello my dear fullstack :P";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
